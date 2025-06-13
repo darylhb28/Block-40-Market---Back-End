e2c1451a395e95b0ec3cba529c73037a0fc443ba
Mason <bevinsm355@gmail.com>
2025-06-12 21:48:14 -0400

    products in progress

--- api/products.js
+++ api/products.js
@@ -1,3 +1,39 @@
 import express from "express"
+import { verifyToken } from "../app"
+import { getProduct, getProducts } from "../db/queries/products";
 const router = express.Router()
+
+router.post('/', verifyToken, async(req, res) =>{
+    const { title, price, description } = req.body;
+    if(!title || !price || !description)
+        return res.status(400).send({error: 'Missing information'});
+
+    const result = await db.query(
+        `INSERT INTO products (title, price, description) VALUES ($1, $2, $3) RETURNING *`,
+        [title, price, description]
+    );
+    res.send(result.rows[0]);
+})
+
+//GET all products
+router.route('/').get(async(req, res) =>{
+    const products = await getProducts();
+    res.send(products);
+});
+
+//GET product by ID
+router.route('/:id').get(async(req, res) =>{
+    const id = Number(req.params.id);
+    if(!Number.isInteger(id) || id <= 0) {
+        return res.status(400).send({error: 'Invalid ID'});
+    }
+
+    const product = await getProduct(id);
+    if(!product)
+        return res.setMaxListeners(404).send({error: 'Product not found'});
+    res.send(product);
+});
+
+
+
 export default router
--- db/queries/products.js
+++ db/queries/products.js
@@ -1,5 +1,24 @@
 import db from "../../db/client.js"
 
+//Get All Products
+export async function getProducts(){
+    const sql =`
+    SELECT * FROM products ORDER BY id;
+    `
+    const {rows} = await db.query(sql);
+    return rows;
+};
+
+//GET Product by ID
+export async function getProduct(id){
+    const sql =`
+    SELECT * FROM products WHERE id = $1;
+    `
+    const {rows} = await db.query(sql, [id]);
+    return rows[0]
+};
+
+//Create Product
 export async function createProduct({title, description, price, image_url}){
 const sql =  `
 INSERT INTO products (title, description, price, image_url)
@@ -8,5 +27,26 @@ RETURNING *
 `
 const {rows: [product]} = await db.query(sql, [title, description, price, image_url])
 return product
-
 }
+
+//Update Products
+export async function updateProduct({ id, title, price, description }){
+    const sql = `
+    UPDATE products
+    SET title = $1, price = $2, decription = $3
+    WHERE id = $4
+    RETURNING *;
+    `
+    const {rows} = await db.quewry(sql, [title, price, description, id]);
+    return rows[0];
+}
+
+//DELETE Products
+export async function deleteProduct(id){
+    const sql =`
+    DELETE FROM products WHERE id = $1 RETURNING *;
+    `
+    const {rows} = await db.query(sql, [id]);
+    return rows[0]
+}
+
